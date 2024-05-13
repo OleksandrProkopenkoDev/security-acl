5ce312143048db533f4d26920e36296ef56ba58c
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-05-13 11:09:05 +0300

    init

---
+++ src/main/java/ua/spro/securityacl/SecurityAclApplication.java
@@ -0,0 +1,13 @@
+package ua.spro.securityacl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAclApplication.class, args);
+	}
+
+}
