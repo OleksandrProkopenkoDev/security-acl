3c6c4743eee4cb3c669c2f22476d5c3bb96849ce
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-05-13 16:01:23 +0300

    step3: use @PreAuthorize with link to Bean: to check if request sender is author of this event

--- src/main/java/ua/spro/securityacl/controller/EventController.java
+++ src/main/java/ua/spro/securityacl/controller/EventController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -69,6 +70,7 @@ public class EventController {
     return mapToEventDto(saved);
   }
 
+  @PreAuthorize("@eventPermissionsService.hasPermissions(authentication, #id)")
   @PatchMapping("/{id}")
   @Transactional
   public EventDto updateEvent(
--- src/main/java/ua/spro/securityacl/security/SecurityConfig.java
+++ src/main/java/ua/spro/securityacl/security/SecurityConfig.java
@@ -4,27 +4,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ua.spro.securityacl.repository.UserRepository;
 
 @Configuration
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
   private final UserRepository userRepository;
-  private final EventPermissionsService eventPermissionsService;
 
   @Bean
-  SecurityFilterChain securityFilterChain(
-      HttpSecurity http) throws Exception {
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.cors(AbstractHttpConfigurer::disable)
         .csrf(AbstractHttpConfigurer::disable)
         .userDetailsService(userDetailsService())
@@ -46,11 +44,7 @@ public class SecurityConfig {
         .exceptionHandling(
             config ->
                 config.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-        .authorizeHttpRequests(config -> config.anyRequest().authenticated())
-        .addFilterBefore(
-            new EventPermissionFilter(
-                eventPermissionsService, new AntPathRequestMatcher("/api/events/**")),
-            AuthorizationFilter.class);
+        .authorizeHttpRequests(config -> config.anyRequest().authenticated());
     return http.build();
   }
 
